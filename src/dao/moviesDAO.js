6079f00827fc8f920a595ffbdd62281f665de248
Somaditya Basak <mail@somaditya.com>
2019-02-18 21:56:00 +0530

    week 1 assignment

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -61,7 +61,8 @@ export default class MoviesDAO {
       // and _id. Do not put a limit in your own implementation, the limit
       // here is only included to avoid sending 46000 documents down the
       // wire.
-      cursor = await movies.find().limit(1)
+      cursor = await movies.find(
+        {countries : {$in : countries} }).project({title:1})
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
@@ -116,7 +117,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Text and Subfield Search
     // Construct a query that will search for the chosen genre.
-    const query = {}
+    const query = { genres : { $in: searchGenre } }
     const project = {}
     const sort = DEFAULT_SORT
 
